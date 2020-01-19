247ac9e9572686089003df7590fa28d279c40492
Jonathan Laasonen <iphntm@iPHNTM.local>
2020-01-19 14:50:40 +0100

    removed testserver dir

--- src/components/ChatPage.js
+++ src/components/ChatPage.js
@@ -1,14 +1,17 @@
 import React, { useEffect } from 'react';
 import socketIOClient from 'socket.io-client';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { setLogin } from '../redux/actions/login';
 
 const socket = socketIOClient('http://localhost:4200');
 
 const Chat = () => {
+    const dispatch = useDispatch();
     const loggedIn = useSelector(state => state.login);
 
     useEffect(() => {
         createMessage(`Welcome, ${loggedIn}!`);
+        socket.connect();
         socket.emit('connecting-user', loggedIn);
     })
 
@@ -23,6 +26,11 @@ const Chat = () => {
     socket.on('chat-message', data => {
         createMessage(`${data.name}: ${data.message}`, 'remote-client');
     })
+
+    const disconnect = () => {
+        // socket.disconnect();
+        dispatch(setLogin(null));
+    };
 
     const preventReload = e => {
         e.preventDefault();
@@ -49,8 +57,10 @@ const Chat = () => {
     }
 
     const appendChatMessage = (message) => {
-        document.querySelector('#chat-board').append(message);
-        autoScrollWindow();
+        if (document.querySelector('#chat-board')){
+            document.querySelector('#chat-board').append(message);
+            autoScrollWindow();
+        } 
     }
 
     const sendMessage = (msg) => {
@@ -70,6 +80,7 @@ const Chat = () => {
                 <button type="submit" className="button" id="chat-send-button" onClick={sendMessage}>Send</button>
                 <span><input type="text" className="text-input" id="chat-input" placeholder="write a message" autoFocus /></span>
             </form>
+            <button id="disconnect-button" className="button" onClick={disconnect}>Disconnect</button>
         </div>
     )
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,9 @@
 import React from 'react';
 import logo from '../logo.png';
-// import socketIOClient from 'socket.io-client';
 import { useDispatch, useSelector } from 'react-redux';
-import { setLogin } from '../redux/actions/login';
-
-// const socket = socketIOClient('http://localhost:4200');
 
 const Header = () => {
-    const dispatch = useDispatch();
     const loggedIn = useSelector(state => state.login);
-
-    const disconnect = () => {
-        // socket.disconnect();
-        dispatch(setLogin(null));
-    };
 
     return (
         <header className='App-header'>
@@ -22,7 +12,6 @@ const Header = () => {
                 <span className='App-name'>SuchChat<span className="trademark">&reg;</span></span>
                 <span className='App-tagline'>- "Much communicate. Very talk. Wow."</span>
             </div>
-            {loggedIn !== null ? <button id="disconnect-button" className="button" onClick={disconnect}>Disconnect</button> : null}
         </header>
     )
 };
