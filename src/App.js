15111297ab329740a731a34cd699cd1d57ff8edc
Jonathan Laasonen <iphntm@iPHNTM.lan>
2020-03-26 18:36:24 +0100

    Restructured chat page component folders

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import './App.css';
-import Header from './components/Header/Header';
-import Chat from './components/ChatPage/ChatPage.tsx';
-import Login from './components/LoginPage/LoginPage';
+import { Header } from './components/Header/Header';
+import { Chat } from './components/ChatPage/ChatPage.tsx';
+import  { Login } from './components/LoginPage/LoginPage';
 import { useSelector } from 'react-redux';
 
 const App = () => {
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,7 +2,7 @@ import React from 'react';
 import logo from '../../logo.png';
 import './Header.css';
 
-const Header = () => {
+export const Header = () => {
     return (
         <header className='App-header'>
             <img src={logo} className='App-logo' alt='logotype' />
@@ -13,5 +13,3 @@ const Header = () => {
         </header>
     )
 };
-
-export default Header;
