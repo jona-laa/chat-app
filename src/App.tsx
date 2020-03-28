371438b4bbf5869bb65d86e65672f9928ff70f2d
Jonathan Laasonen <iphntm@iPHNTM.lan>
2020-03-28 22:09:06 +0100

    interface for updateLogin params

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import './App.css';
-import { Header } from './components/Header/Header';
-import { Chat } from './components/ChatPage/ChatPage.tsx';
-import  { Login } from './components/LoginPage/LoginPage';
+import { Header } from './components/Header/Header';
+import { Chat } from './components/ChatPage/ChatPage';
+import { Login } from './components/LoginPage/LoginPage';
 import { useSelector } from 'react-redux';
 
-const App = () => {
+const App: React.FC = (): JSX.Element => {
   const loggedIn = useSelector(state => state.login);
   return (
     <div className="App">
